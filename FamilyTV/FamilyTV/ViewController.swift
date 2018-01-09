0b47ce037a2da19ed5024299b3ffcadaba00ac79
brothernguyen <tuananh3@gmail.com>
2018-01-09 19:57:46 +0700

    Added CollectionView

--- FamilyTV/FamilyTV/ViewController.swift
+++ FamilyTV/FamilyTV/ViewController.swift
@@ -1,25 +1,29 @@
 //
 //  ViewController.swift
-//  FamilyTV
+//  Family TV
 //
-//  Created by Tuan Anh on 1/9/18.
+//  Created by Tuan Anh on 1/8/18.
 //  Copyright © 2018 Tuan Anh. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
     }
-
-
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        
+        return cell
+    }
+    
 }
 
