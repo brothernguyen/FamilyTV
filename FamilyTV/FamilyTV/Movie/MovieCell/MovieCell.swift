b77c1e14b64d596398df3fc4ffbdd09b222d4384
Neil Tran <neil.tran@terralogic.com>
2018-02-07 18:08:16 +0700

    Reuseable movie cell

---
+++ FamilyTV/FamilyTV/Movie/MovieCell/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCellCollectionViewCell.swift
+//  FamilyTV
+//
+//  Created by Tuan Anh on 1/30/18.
+//  Copyright © 2018 Tuan Anh. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+    
+    @IBOutlet weak var loadingImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.loadingImg.adjustsImageWhenAncestorFocused = true
+    }
+    
+    
+    override var canBecomeFocused : Bool {
+        return true
+    }
+}
--- FamilyTV/FamilyTV/Movie/MovieCollectionView.swift
+++ FamilyTV/FamilyTV/Movie/MovieCollectionView.swift
@@ -8,13 +8,21 @@
 
 import UIKit
 
-class MovieCollectionView: UIView {
+class MovieCollectionView: UIView, UICollectionViewDelegate, UICollectionViewDataSource {
     
     @IBOutlet var contentView: UIView!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        self.collectionView.register(UINib(nibName: "MovieCell", bundle:nil), forCellWithReuseIdentifier:"MovieCell");
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -25,12 +33,22 @@ class MovieCollectionView: UIView {
     private func commonInit() {
         Bundle.main.loadNibNamed("MovieCollectionView", owner: self, options: nil) 
         self.addSubview(contentView)
-        contentView.translatesAutoresizingMaskIntoConstraints = false
         
-        contentView.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        contentView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-        contentView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
-        contentView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+//        contentView.translatesAutoresizingMaskIntoConstraints = false
+//        contentView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+//        contentView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+//        contentView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+//        contentView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let newsCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCell", for: indexPath) as? MovieCell else { fatalError("Couldn't dequeue a cell") }
+        
+        return newsCell
     }
     
 }
